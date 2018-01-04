ab596d0d4141d5f44f7805e9db88fc38a6637298
yedaodao <404069912@qq.com>
2018-01-04 23:01:09 +0800

    add correct dll demo

--- build-scripts/webpack.prod.js
+++ build-scripts/webpack.prod.js
@@ -1,36 +1,9 @@
-const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
 
-module.exports = {
-    entry: {
-        app: ['./app.js']
-    },
-    output: {
-        path: path.join(__dirname, '../build'),
-        filename: 'bundle.[hash].js',
-        publicPath: "/"
-    },
-    module: {
-        rules:  [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules(\/|\\)attr-accept|node_modules(\/|\\)disposables)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        cacheDirectory: true,
-                        presets: ['env'],
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            inject: 'body',
-            filename: 'test-log.html',
-            template: path.join(__dirname, '../src/apps/for-log/test-log.html')
-        })
-    ]
-};
+const baseWebpackConfig = require('./webpack.base');
+
+module.exports = merge(baseWebpackConfig, {
+    devtool: false,
+    plugins: []
+});
